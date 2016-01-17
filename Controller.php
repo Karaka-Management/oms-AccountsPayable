8ecb146a6e37c595c5a8fed82246ebaa7683d16c
Dennis Eichhorn <spl1nes.com@googlemail.com>
2016-01-17 18:31:49 +0100

    Fix dependencies

--- Controller.php
+++ Controller.php
@@ -107,7 +107,6 @@ class Controller extends ModuleAbstract implements WebInterface
      * @since 1.0.0
      */
     protected static $dependencies = [
-        'Admin',
         'Media',
     ];
 
