1aa3d5a043066c6874e583b6862156240186f65f
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-06-24 10:51:43 +0200

    Fixing templates

--- Theme/Backend/creditor-list.tpl.php
+++ Theme/Backend/creditor-list.tpl.php
@@ -26,27 +26,31 @@ $footerView->setResults(1);
 
 echo $this->getData('nav')->render(); ?>
 
-<div class="box w-100">
-    <table class="table red">
-        <caption><?= $this->getText('AccountsPayable') ?></caption>
-        <thead>
-        <tr>
-            <td><?= $this->getText('ID', 0, 0); ?>
-            <td><?= $this->getText('Name1'); ?>
-            <td><?= $this->getText('Name2'); ?>
-            <td class="wf-100"><?= $this->getText('Name3'); ?>
-            <td><?= $this->getText('City'); ?>
-            <td><?= $this->getText('Zip'); ?>
-            <td><?= $this->getText('Address'); ?>
-            <td><?= $this->getText('Country'); ?>
-        <tfoot>
-        <tr>
-            <td colspan="8"><?= $footerView->render(); ?>
-        <tbody>
-        <?php $count = 0; foreach([] as $key => $value) : $count++; ?>
-        <?php endforeach; ?>
-        <?php if($count === 0) : ?>
-        <tr><td colspan="8" class="empty"><?= $this->getText('Empty', 0, 0); ?>
-                <?php endif; ?>
-    </table>
+<div class="row">
+    <div class="col-xs-12">
+        <div class="box wf-100">
+            <table class="table red">
+                <caption><?= $this->getText('AccountsPayable') ?></caption>
+                <thead>
+                <tr>
+                    <td><?= $this->getText('ID', 0, 0); ?>
+                    <td><?= $this->getText('Name1'); ?>
+                    <td><?= $this->getText('Name2'); ?>
+                    <td class="wf-100"><?= $this->getText('Name3'); ?>
+                    <td><?= $this->getText('City'); ?>
+                    <td><?= $this->getText('Zip'); ?>
+                    <td><?= $this->getText('Address'); ?>
+                    <td><?= $this->getText('Country'); ?>
+                <tfoot>
+                <tr>
+                    <td colspan="8"><?= $footerView->render(); ?>
+                <tbody>
+                <?php $count = 0; foreach([] as $key => $value) : $count++; ?>
+                <?php endforeach; ?>
+                <?php if($count === 0) : ?>
+                <tr><td colspan="8" class="empty"><?= $this->getText('Empty', 0, 0); ?>
+                        <?php endif; ?>
+            </table>
+        </div>
+    </div>
 </div>
