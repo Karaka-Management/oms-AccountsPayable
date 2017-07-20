658b523b2cca1761e317d60aa2d56153b1e1330a
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-07-20 18:12:29 +0200

    Remove @author docblock

--- Controller.php
+++ Controller.php
@@ -7,7 +7,6 @@
  * @category   TBD
  * @package    TBD
  * @author     OMS Development Team <dev@oms.com>
- * @author     Dennis Eichhorn <d.eichhorn@oms.com>
  * @copyright  Dennis Eichhorn
  * @license    OMS License 1.0
  * @version    1.0.0
@@ -32,7 +31,6 @@ use phpOMS\Views\ViewLayout;
  * @category   Modules
  * @package    Modules\Accountsreceivable
  * @author     OMS Development Team <dev@oms.com>
- * @author     Dennis Eichhorn <d.eichhorn@oms.com>
  * @license    OMS License 1.0
  * @link       http://orange-management.com
  * @since      1.0.0
@@ -90,7 +88,6 @@ class Controller extends ModuleAbstract implements WebInterface
      * @return RenderableInterface
      *
      * @since  1.0.0
-     * @author Dennis Eichhorn <d.eichhorn@oms.com>
      */
     public function viewCreditorList(RequestAbstract $request, ResponseAbstract $response, $data = null) : \Serializable
     {
@@ -109,7 +106,6 @@ class Controller extends ModuleAbstract implements WebInterface
      * @return RenderableInterface
      *
      * @since  1.0.0
-     * @author Dennis Eichhorn <d.eichhorn@oms.com>
      */
     public function viewCreditorCreate(RequestAbstract $request, ResponseAbstract $response, $data = null) : \Serializable
     {
@@ -128,7 +124,6 @@ class Controller extends ModuleAbstract implements WebInterface
      * @return RenderableInterface
      *
      * @since  1.0.0
-     * @author Dennis Eichhorn <d.eichhorn@oms.com>
      */
     public function viewCreditorProfile(RequestAbstract $request, ResponseAbstract $response, $data = null) : \Serializable
     {
@@ -147,7 +142,6 @@ class Controller extends ModuleAbstract implements WebInterface
      * @return RenderableInterface
      *
      * @since  1.0.0
-     * @author Dennis Eichhorn <d.eichhorn@oms.com>
      */
     public function viewEntriesList(RequestAbstract $request, ResponseAbstract $response, $data = null) : \Serializable
     {
@@ -166,7 +160,6 @@ class Controller extends ModuleAbstract implements WebInterface
      * @return RenderableInterface
      *
      * @since  1.0.0
-     * @author Dennis Eichhorn <d.eichhorn@oms.com>
      */
     public function viewAnalyzeDashboard(RequestAbstract $request, ResponseAbstract $response, $data = null) : \Serializable
     {
