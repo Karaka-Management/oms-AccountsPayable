906066183f1e1b883171490319dea2cb9dea7a7f
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-07-26 17:48:08 +0200

    Fix file author and copyright

--- Controller.php
+++ Controller.php
@@ -6,7 +6,6 @@
  *
  * @category   TBD
  * @package    TBD
- * @author     OMS Development Team <dev@oms.com>
  * @copyright  Dennis Eichhorn
  * @license    OMS License 1.0
  * @version    1.0.0
@@ -30,7 +29,6 @@ use phpOMS\Views\ViewLayout;
  *
  * @category   Modules
  * @package    Modules\Accountsreceivable
- * @author     OMS Development Team <dev@oms.com>
  * @license    OMS License 1.0
  * @link       http://orange-management.com
  * @since      1.0.0
