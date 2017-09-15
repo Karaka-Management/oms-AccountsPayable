199b55ec9ceea6e9d128ea808c83852fcc3e3e9f
Dennis Eichhorn <spl1nes.com@googlemail.com>
2017-09-15 19:37:34 +0200

    Add module id to modules

--- Controller.php
+++ Controller.php
@@ -61,6 +61,14 @@ class Controller extends ModuleAbstract implements WebInterface
     /* public */ const MODULE_NAME = 'AccountsPayable';
 
     /**
+     * Module id.
+     *
+     * @var int
+     * @since 1.0.0
+     */
+    /* public */ const MODULE_ID = 1005000000;
+
+    /**
      * Providing.
      *
      * @var string
