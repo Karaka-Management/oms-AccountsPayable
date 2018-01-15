5c84631328d35c8632363c8f365f60e21a22cbf8
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-01-15 18:56:01 +0100

    Remove whitespace from end of line

--- Admin/Routes/Web/Backend.php
+++ Admin/Routes/Web/Backend.php
@@ -5,55 +5,55 @@ use phpOMS\Router\RouteVerb;
 return [
     '^.*/backend/accounting/payable/list.*$' => [
         [
-            'dest' => '\Modules\AccountsPayable\Controller:viewCreditorList', 
+            'dest' => '\Modules\AccountsPayable\Controller:viewCreditorList',
             'verb' => RouteVerb::GET,
         ],
     ],
     '^.*/backend/accounting/payable/create.*$' => [
         [
-            'dest' => '\Modules\AccountsPayable\Controller:viewCreditorCreate', 
+            'dest' => '\Modules\AccountsPayable\Controller:viewCreditorCreate',
             'verb' => RouteVerb::GET,
         ],
     ],
     '^.*/backend/accounting/payable/profile.*$' => [
         [
-            'dest' => '\Modules\AccountsPayable\Controller:viewCreditorProfile', 
+            'dest' => '\Modules\AccountsPayable\Controller:viewCreditorProfile',
             'verb' => RouteVerb::GET,
         ],
     ],
     '^.*/backend/accounting/payable/outstanding.*$' => [
         [
-            'dest' => '\Modules\AccountsPayable\Controller:viewCreditorOutstanding', 
+            'dest' => '\Modules\AccountsPayable\Controller:viewCreditorOutstanding',
             'verb' => RouteVerb::GET,
         ],
     ],
     '^.*/backend/accounting/payable/age.*$' => [
         [
-            'dest' => '\Modules\AccountsPayable\Controller:viewCreditorAge', 
+            'dest' => '\Modules\AccountsPayable\Controller:viewCreditorAge',
             'verb' => RouteVerb::GET,
         ],
     ],
     '^.*/backend/accounting/payable/payable.*$' => [
         [
-            'dest' => '\Modules\AccountsPayable\Controller:viewCreditorPayable', 
+            'dest' => '\Modules\AccountsPayable\Controller:viewCreditorPayable',
             'verb' => RouteVerb::GET,
         ],
     ],
     '^.*/backend/accounting/payable/journal/list.*$' => [
         [
-            'dest' => '\Modules\AccountsPayable\Controller:viewJournalList', 
+            'dest' => '\Modules\AccountsPayable\Controller:viewJournalList',
             'verb' => RouteVerb::GET,
         ],
     ],
     '^.*/backend/accounting/payable/entries.*$' => [
         [
-            'dest' => '\Modules\AccountsPayable\Controller:viewEntriesList', 
+            'dest' => '\Modules\AccountsPayable\Controller:viewEntriesList',
             'verb' => RouteVerb::GET,
         ],
     ],
     '^.*/backend/accounting/payable/analyze.*$' => [
         [
-            'dest' => '\Modules\AccountsPayable\Controller:viewAnalyzeDashboard', 
+            'dest' => '\Modules\AccountsPayable\Controller:viewAnalyzeDashboard',
             'verb' => RouteVerb::GET,
         ],
     ],
