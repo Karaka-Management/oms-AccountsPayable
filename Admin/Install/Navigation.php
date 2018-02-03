e0bea822a20ec663c48a034bad3017b7c54435e1
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-02-03 11:46:23 +0100

    Add spaces around namespace

--- Admin/Install/Navigation.php
+++ Admin/Install/Navigation.php
@@ -11,7 +11,9 @@
  * @link       http://website.orange-management.de
  */
 declare(strict_types = 1);
+
 namespace Modules\AccountsPayable\Admin\Install;
+
 use phpOMS\DataStorage\Database\DatabasePool;
 
 /**
--- Admin/Uninstall.php
+++ Admin/Uninstall.php
@@ -11,6 +11,7 @@
  * @link       http://website.orange-management.de
  */
 declare(strict_types = 1);
+
 namespace Modules\AccountsPayable\Admin;
 
 use phpOMS\DataStorage\Database\DatabasePool;
