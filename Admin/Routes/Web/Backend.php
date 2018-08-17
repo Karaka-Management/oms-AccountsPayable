e3186404e314d1ada7f0d43a19406ce8d3c959ba
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-08-17 20:02:29 +0200

    fix #96 fix #97

--- Admin/Routes/Web/Backend.php
+++ Admin/Routes/Web/Backend.php
@@ -1,60 +1,108 @@
 <?php
 
 use phpOMS\Router\RouteVerb;
+use phpOMS\Account\PermissionType;
+use Modules\AccountsPayable\Models\PermissionState;
+use Modules\AccountsPayable\Controller;
 
 return [
     '^.*/backend/accounting/payable/list.*$' => [
         [
             'dest' => '\Modules\AccountsPayable\Controller:viewCreditorList',
             'verb' => RouteVerb::GET,
+            'permission' => [
+                'module' => Controller::MODULE_NAME,
+                'type'  => PermissionType::READ,
+                'state' => PermissionState::PAYABLE,
+            ],
         ],
     ],
     '^.*/backend/accounting/payable/create.*$' => [
         [
             'dest' => '\Modules\AccountsPayable\Controller:viewCreditorCreate',
             'verb' => RouteVerb::GET,
+            'permission' => [
+                'module' => Controller::MODULE_NAME,
+                'type'  => PermissionType::CREATE,
+                'state' => PermissionState::PAYABLE,
+            ],
         ],
     ],
     '^.*/backend/accounting/payable/profile.*$' => [
         [
             'dest' => '\Modules\AccountsPayable\Controller:viewCreditorProfile',
             'verb' => RouteVerb::GET,
+            'permission' => [
+                'module' => Controller::MODULE_NAME,
+                'type'  => PermissionType::READ,
+                'state' => PermissionState::PAYABLE,
+            ],
         ],
     ],
     '^.*/backend/accounting/payable/outstanding.*$' => [
         [
             'dest' => '\Modules\AccountsPayable\Controller:viewCreditorOutstanding',
             'verb' => RouteVerb::GET,
+            'permission' => [
+                'module' => Controller::MODULE_NAME,
+                'type'  => PermissionType::READ,
+                'state' => PermissionState::PAYABLE,
+            ],
         ],
     ],
     '^.*/backend/accounting/payable/age.*$' => [
         [
             'dest' => '\Modules\AccountsPayable\Controller:viewCreditorAge',
             'verb' => RouteVerb::GET,
+            'permission' => [
+                'module' => Controller::MODULE_NAME,
+                'type'  => PermissionType::READ,
+                'state' => PermissionState::PAYABLE,
+            ],
         ],
     ],
     '^.*/backend/accounting/payable/payable.*$' => [
         [
             'dest' => '\Modules\AccountsPayable\Controller:viewCreditorPayable',
             'verb' => RouteVerb::GET,
+            'permission' => [
+                'module' => Controller::MODULE_NAME,
+                'type'  => PermissionType::READ,
+                'state' => PermissionState::PAYABLE,
+            ],
         ],
     ],
     '^.*/backend/accounting/payable/journal/list.*$' => [
         [
             'dest' => '\Modules\AccountsPayable\Controller:viewJournalList',
             'verb' => RouteVerb::GET,
+            'permission' => [
+                'module' => Controller::MODULE_NAME,
+                'type'  => PermissionType::READ,
+                'state' => PermissionState::PAYABLE,
+            ],
         ],
     ],
     '^.*/backend/accounting/payable/entries.*$' => [
         [
             'dest' => '\Modules\AccountsPayable\Controller:viewEntriesList',
             'verb' => RouteVerb::GET,
+            'permission' => [
+                'module' => Controller::MODULE_NAME,
+                'type'  => PermissionType::READ,
+                'state' => PermissionState::PAYABLE,
+            ],
         ],
     ],
     '^.*/backend/accounting/payable/analyze.*$' => [
         [
             'dest' => '\Modules\AccountsPayable\Controller:viewAnalyzeDashboard',
             'verb' => RouteVerb::GET,
+            'permission' => [
+                'module' => Controller::MODULE_NAME,
+                'type'  => PermissionType::READ,
+                'state' => PermissionState::PAYABLE,
+            ],
         ],
     ],
 ];
---
+++ Models/PermissionState.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.2
+ *
+ * @package    Modules\AccountsPayable
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://website.orange-management.de
+ */
+declare(strict_types=1);
+
+namespace Modules\AccountsPayable\Models;
+
+use phpOMS\Stdlib\Base\Enum;
+
+/**
+ * Permision state enum.
+ *
+ * @package    Modules\AccountsPayable
+ * @license    OMS License 1.0
+ * @link       http://website.orange-management.de
+ * @since      1.0.0
+ */
+abstract class PermissionState extends Enum
+{
+    public const PAYABLE = 1;
+}
