c3abdf5bab51d2ac31d5cd39f56b7cf0a3dc78ec
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-12-22 19:51:26 +0100

    Move schema to json

--- Admin/Installer.php
+++ Admin/Installer.php
@@ -29,45 +29,4 @@ use phpOMS\Module\InstallerAbstract;
  */
 class Installer extends InstallerAbstract
 {
-
-    /**
-     * {@inheritdoc}
-     */
-    public static function install(DatabasePool $dbPool, InfoManager $info) : void
-    {
-        parent::install($dbPool, $info);
-
-        switch ($dbPool->get()->getType()) {
-            case DatabaseType::MYSQL:
-                $dbPool->get()->con->prepare(
-                    'CREATE TABLE if NOT EXISTS `' . $dbPool->get()->prefix . 'accounts_payable` (
-                            `accounts_payable_id` int(11) NOT NULL AUTO_INCREMENT,
-                            `accounts_payable_account` int(11) DEFAULT NULL,
-                            PRIMARY KEY (`accounts_payable_id`),
-                            KEY `accounts_payable_account` (`accounts_payable_account`)
-                        )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;'
-                )->execute();
-
-                $dbPool->get()->con->prepare(
-                    'ALTER TABLE `' . $dbPool->get()->prefix . 'accounts_payable`
-                            ADD CONSTRAINT `' . $dbPool->get()->prefix . 'accounts_payable_ibfk_1` FOREIGN KEY (`accounts_payable_account`) REFERENCES `' . $dbPool->get()->prefix . 'account` (`account_id`);'
-                )->execute();
-
-                $dbPool->get()->con->prepare(
-                    'CREATE TABLE if NOT EXISTS `' . $dbPool->get()->prefix . 'accounts_payable_payment` (
-                            `accounts_payable_payment_id` int(11) NOT NULL AUTO_INCREMENT,
-                            `accounts_payable_payment_account` int(11) DEFAULT NULL,
-                            `accounts_payable_payment_info` int(11) DEFAULT NULL,
-                            PRIMARY KEY (`accounts_payable_payment_id`),
-                            KEY `accounts_payable_payment_account` (`accounts_payable_payment_account`)
-                        )ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;'
-                )->execute();
-
-                $dbPool->get()->con->prepare(
-                    'ALTER TABLE `' . $dbPool->get()->prefix . 'accounts_payable_payment`
-                            ADD CONSTRAINT `' . $dbPool->get()->prefix . 'accounts_payable_payment_ibfk_1` FOREIGN KEY (`accounts_payable_payment_account`) REFERENCES `' . $dbPool->get()->prefix . 'accounts_payable` (`accounts_payable_id`);'
-                )->execute();
-                break;
-        }
-    }
 }
